1c168576c5014c439d5495dfdcc74afd91d601d4
sipocharles@yahoo.co.uk <sipocharles@yahoo.co.uk>
2019-03-24 01:36:33 +0000

    updated code page typography, cleared user.txt, added favicon

--- run.py
+++ run.py
@@ -7,24 +7,9 @@ app = Flask(__name__)
 with open("data/highscores.json", "r") as json_data:
     highscores = json.load(json_data)
 
-#highscores = [(10, 'Sipo'), (5, 'Matt')]
-#print(highscores)
+
 player_score = 0
-# global userTry
-# userTry = 0
 
-# result = {
-#                 "name" : "ZB",
-#                 "score": 10
-#             } 
-# print(result)
-# highscores.append(result)
-# #toWrite = json.dump(highscores)
-# print(highscores)
-
-# with open("data/highscores.json", "w") as file:
-#     json.dump(highscores, file)
-    #file.writelines(str(toWrite))
 
 def write_to_file(filename, data):
     with open(filename, "a") as file:
@@ -33,10 +18,8 @@ def write_to_file(filename, data):
 
 def add_users(username):
     write_to_file("data/users.txt", username.title() + "\n")
-    #username  = username
 
 
-#username = ""
 riddle_index = 0
 riddles = ""
 
@@ -50,7 +33,7 @@ def index():
 def leaderboard():
     with open("data/highscores.json", "r") as json_data:
         highscores = json.load(json_data)
-    return render_template("leaderboard.html", title="Leaderboard | Space Riddle", highscores = highscores)
+    return render_template("leaderboard.html", title="Leaderboard | Space Riddle", highscores=highscores)
 
 
 @app.route('/enterName', methods=["GET", "POST"])
@@ -58,10 +41,10 @@ def enterName():
     if request.method == "POST":
         username = request.form["username"]
         add_users(username)
-        #redirecting before
+
         return redirect(username)
-       #return render_template("answerRiddle.html", title="Answer Riddle | Space Riddle", username=username, riddle_index=riddle_index, riddles=riddles)
-    return render_template("enterName.html", title="Enter Name | Space Riddle")
+
+    return render_template("enterName.html", title="User Registration | Space Riddle")
 
 
 @app.route('/<username>', methods=["GET", "POST"])
@@ -78,58 +61,51 @@ def play(username):
         riddle_index = int(request.form["riddle_index"])
         player_score = int(request.form["score"])
         user_answer = request.form["answer"].lower()
-        
+
         user_Try = int(request.form["userTry"])
 
         print("user try: " + str(user_Try))
 
         if user_answer in riddles[riddle_index]["answer"]:
-            # Correct answer
-            # Go to next riddle
+
             riddle_index += 1
             player_score += 1
             incorrect = ''
         else:
             print("user try in else: " + str(user_Try))
             print(type(user_Try))
-            
+
             if(user_Try == 1):
-                user_Try -=1
+                user_Try -= 1
                 print("user try in other: " + str(user_Try))
                 riddle_index += 1
 
             elif(user_Try == 0):
                 incorrect = user_answer
                 user_Try += 1
-            
-            
-            
 
         if riddle_index > 5:
-            #highscores.append((player_score, username.title()))
-            #highscores.sort(reverse=True)
-            #highscores.append({"name": username.title(), "score": player_score})
-            
-            
+
             result = {
-                "name" : username,
+                "name": username,
                 "score": player_score
-            } 
+            }
 
             with open("data/highscores.json", "r") as json_data:
                 highscores = json.load(json_data)
 
             highscores.append(result)
-            highscores = sorted(highscores, key=itemgetter('score'), reverse=True)
+            highscores = sorted(
+                highscores, key=itemgetter('score'), reverse=True)
             highscores = highscores[:-1]
             print(highscores)
 
             with open("data/highscores.json", "w") as file:
                 json.dump(highscores, file)
 
-            return render_template("leaderboard.html", title="Game Over", score = player_score, highscores=highscores)
-        #print(username)
-    return render_template("answerRiddle.html", title="Play Game | Space Riddle", username=username, riddles=riddles, riddle_index=riddle_index, score=player_score, incorrect = incorrect, userTry = user_Try)
+            return render_template("leaderboard.html", title="Game Over", score=player_score, highscores=highscores)
+
+    return render_template("answerRiddle.html", title="Play Game | Space Riddle", username=username, riddles=riddles, riddle_index=riddle_index, score=player_score, incorrect=incorrect, userTry=user_Try)
 
 
 if __name__ == '__main__':
